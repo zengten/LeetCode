7233ef0c7753f0098c64269664b704888be81714
GitHub Action <action@github.com>
2025-03-16 16:20:14 +0000

    test: [20250317] Add daily LeetCode problem

---
+++ problems/problems_1963/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.minSwaps(test_input)
+
+    def minSwaps(self, s: str) -> int:
+        pass
+
